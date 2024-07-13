45899381b661434c9d9783cb460df451061db47b
Rishub C R (Craftsman) <54368593+magusCoder-official@users.noreply.github.com>
2024-07-13 15:42:17 +0530

    Add host and sub host details to event creation and sign up

--- meetup_clone/app/forms.py
+++ meetup_clone/app/forms.py
@@ -7,11 +7,16 @@ class EventForm(FlaskForm):
     description = TextAreaField('Description')
     date = DateTimeField('Date and Time', format='%Y-%m-%d %H:%M', validators=[DataRequired()])
     location = StringField('Location', validators=[Length(max=100)])
+    host_email = StringField('Host Email', validators=[DataRequired(), Email()])
+    host_phone = StringField('Host Phone', validators=[DataRequired(), Length(max=15)])
+    sub_host_email = StringField('Sub Host Email', validators=[Email()])
+    sub_host_phone = StringField('Sub Host Phone', validators=[Length(max=15)])
     submit = SubmitField('Create Event')
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
     email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = StringField('Phone', validators=[DataRequired(), Length(max=15)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Sign Up')
 
--- meetup_clone/app/models.py
+++ meetup_clone/app/models.py
@@ -7,6 +7,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
     password_hash = db.Column(db.String(128))
     events = db.relationship('Event', backref='organizer', lazy='dynamic')
 
@@ -22,6 +23,10 @@ class Event(db.Model):
     description = db.Column(db.Text)
     date = db.Column(db.DateTime, nullable=False)
     location = db.Column(db.String(100))
+    host_email = db.Column(db.String(120), nullable=False)
+    host_phone = db.Column(db.String(15), nullable=False)
+    sub_host_email = db.Column(db.String(120))
+    sub_host_phone = db.Column(db.String(15))
     organizer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 @login_manager.user_loader
